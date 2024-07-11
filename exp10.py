f63d35ecff517204945cf8aa451bc050fcfbf682
Ajay Varma <128949987+ajayvarma007789@users.noreply.github.com>
2024-07-11 20:34:18 +0530

    Update exp10.py

--- exp10.py
+++ exp10.py
@@ -1,9 +1,6 @@
 #factorial of a given Natural Number n using the recursive functions
 def factorial(n):
-     #if (n==1 or n==0):
-     #     return 1
-     #else:
-     #     n * factorial(n - 1)
+    
     return 1 if (n==1 or n==0) else n * factorial(n - 1) 
  
 
